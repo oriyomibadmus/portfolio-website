300c889baaf279d9c069cb330e8f13c37eb1111e
oriyomibadmus <badmus.oriyomi1@gmail.com>
2023-03-28 14:46:51 +0400

    My First Commit

---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,108 @@
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+const MySkills = () => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      {/* HEADER AND IMAGE SECTION */}
+      <div className="md:flex md:justify-between md:gap-16 mt-32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            MY <span className="text-red">FORMULA</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+          THE M.E.R.N. STACK is my preferred revitalizing potion. I have always held a strong view that I MUST LEARN TO BUILD AND DEPLOY an entire web application on my own. To accomplish that goal, I TAUGHT MYSELF MongoDB, Express, React, Node - the complete M.E.R.N. stack.
+          </p>
+        </motion.div>
+
+        <div className="mt-16 md:mt-0">
+          {isAboveLarge ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+              before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                className="z-10"
+                src="assets/skills-image.png"
+              />
+            </div>
+          ) : (
+            <img alt="skills" className="z-10" src="assets/skills-image.png" />
+          )}
+        </div>
+      </div>
+
+      {/* SKILLS */}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* FULLSTACK */}
+        <motion.div
+          className="md:w-1/2 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Fullstack
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            MongoDB, Express, React, and Node are all used in full-stack MERN development to create a web application from scratch. It is scalable, adaptable, and simple to use.
+          </p>
+        </motion.div>
+
+        {/* BACKEND */}
+        <motion.div
+          className="md:w-1/2 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Backend
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+          Developing a web application's server-side, which includes server-side logic, databases, and APIs. Performance, scalability, and technology all depend on it:  Node.js 
+
+          </p>
+        </motion.div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default MySkills;
